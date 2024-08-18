146105602fb942c7bdcefe7742c1cfc3b1986fda
SD Asif Hossein <s.dah.ingularity47@gmail.com>
2024-08-18 22:31:19 +0600

    Supply the traceback function to use against xpcall

--- pt-lua.c
+++ pt-lua.c
@@ -983,6 +983,10 @@ int main (int argc, char **argv) {
   /* -------- PALLENE TRACER CODE -------- */
   (void) pallene_tracer_init(L);  /* initialize pallene tracer */
   lua_pop(L, 1);  /* We do not need the finalizer object here */
+
+  /* supply the message handler function with custom tracebacks. */
+  lua_pushcfunction(L, msghandler);
+  lua_setglobal(L, "pallene_tracer_errhandler");
   /* -------- PALLENE TRACER CODE END -------- */
 
   lua_pushcfunction(L, &pmain);  /* to call 'pmain' in protected mode */
